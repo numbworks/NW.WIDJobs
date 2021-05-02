23951a0d9b2161e3824c39ee1820e76337522859
Rubèn <numbworks@gmail.com>
2021-05-03 00:37:33 +0200

    Validator: added.

--- src/NW.WebsiteExploration/AWebsiteExplorer.cs
+++ src/NW.WebsiteExploration/AWebsiteExplorer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Threading;
-using RUBN.Shared;
 
 namespace NW.WebsiteExploration
 {
---
+++ src/NW.WebsiteExploration/Messages/MessageCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NW.WebsiteExploration
+{
+    public static class MessageCollection
+    {
+
+        // Validator
+        public static Func<string, string, string> Validator_FirstValueIsGreaterOrEqualThanSecondValue
+            = (variableName1, variableName2) => $"The '{variableName1}''s value is greater or equal than '{variableName2}''s value.";
+        public static Func<string, string, string> Validator_FirstValueIsGreaterThanSecondValue
+            = (variableName1, variableName2) => $"The '{variableName1}''s value is greater than '{variableName2}''s value.";
+        public static Func<string, string> Validator_VariableContainsZeroItems
+            = (variableName) => $"'{variableName}' contains zero items.";
+        public static Func<string, string> Validator_VariableCantBeLessThanOne
+            = (variableName) => $"'{variableName}' can't be less than one.";
+        public static Func<string, string, string> Validator_DividingMustReturnWholeNumber { get; }
+            = (variableName1, variableName2) => $"Dividing '{variableName1}' by '{variableName2}' must return a whole number.";
+
+        private static string RollOutCollection(List<double> coll)
+            => RollOutCollection(coll.Cast<object>().ToList());
+        private static string RollOutCollection(IEnumerable<object> coll)
+        {
+
+            List<string> list = new List<string>();
+
+            foreach (object obj in coll)
+                list.Add(obj.ToString());
+
+            return $"[{string.Join(", ", list)}]";
+
+        }
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 29.04.2021
+
+*/
