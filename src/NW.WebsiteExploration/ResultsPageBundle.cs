f69680e795bf4c85e021bf4994ef9833808f62e4
Rubèn <numbworks@gmail.com>
2021-05-02 23:52:43 +0200

    ResultsPageBundle: added.

---
+++ src/NW.WebsiteExploration/ResultsPageBundle.cs
@@ -0,0 +1,22 @@
+﻿namespace NW.WebsiteExploration
+{
+    public class ResultsPageBundle
+    {
+
+        // Fields
+        // Properties
+        public string Response { get; set; }
+        public ResultsPage Page { get; set; }
+
+        // Constructors
+        // Methods
+
+    }
+}
+
+/*
+ *
+ *  Author: numbworks@gmail.com
+ *  Last Update: 22.09.2018
+ * 
+ */
