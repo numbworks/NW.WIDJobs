cce72fbfd1e2635e510eb5591843b49470aafe1c
Rubèn <numbworks@gmail.com>
2021-05-10 01:47:42 +0200

    WIDExplorerComponents: updated.

--- src/NW.WIDJobs/WIDExplorerComponents.cs
+++ src/NW.WIDJobs/WIDExplorerComponents.cs
@@ -13,28 +13,65 @@ namespace NW.WIDJobs
         public Action<string> LoggingAction { get; }
         public IXPathManager XPathManager { get; }
         public IGetRequestManager GetRequestManager { get; }
+        public IPageManager PageManager { get; }
+        public IPageScraper PageScraper { get; }
+        public IPageItemScraper PageItemScraper { get; }
+        public IPageItemExtendedManager PageItemExtendedManager { get; }
+        public IPageItemExtendedScraper PageItemExtendedScraper { get; }
+        public IRunIdManager RunIdManager { get; }
+        public IBulletPointManager BulletPointManager { get; }
 
         // Constructors	
         public WIDExplorerComponents(
             Action<string> loggingAction, 
             IXPathManager xpathManager, 
-            IGetRequestManager getRequestManager)
+            IGetRequestManager getRequestManager,
+            IPageManager pageManager,
+            IPageScraper pageScraper,
+            IPageItemScraper pageItemScraper,
+            IPageItemExtendedManager pageItemExtendedManager,
+            IPageItemExtendedScraper pageItemExtendedScraper,
+            IRunIdManager runIdManager,
+            IBulletPointManager bulletPointManager
+            )
         {
 
             Validator.ValidateObject(loggingAction, nameof(loggingAction));
             Validator.ValidateObject(xpathManager, nameof(xpathManager));
             Validator.ValidateObject(getRequestManager, nameof(getRequestManager));
+            Validator.ValidateObject(pageManager, nameof(pageManager));
+            Validator.ValidateObject(pageScraper, nameof(pageScraper));
+            Validator.ValidateObject(pageItemScraper, nameof(pageItemScraper));
+            Validator.ValidateObject(pageItemExtendedManager, nameof(pageItemExtendedManager));
+            Validator.ValidateObject(pageItemExtendedScraper, nameof(pageItemExtendedScraper));
+            Validator.ValidateObject(runIdManager, nameof(runIdManager));
+            Validator.ValidateObject(bulletPointManager, nameof(bulletPointManager));
 
             LoggingAction = loggingAction;
             XPathManager = xpathManager;
             GetRequestManager = getRequestManager;
+            PageManager = pageManager;
+            PageScraper = pageScraper;
+            PageItemScraper = pageItemScraper;
+            PageItemExtendedManager = pageItemExtendedManager;
+            PageItemExtendedScraper = pageItemExtendedScraper;
+            RunIdManager = runIdManager;
+            BulletPointManager = bulletPointManager;
 
         }
         public WIDExplorerComponents()
             : this(
                   DefaultLoggingAction,
                   new XPathManager(), 
-                  new GetRequestManager()) { }
+                  new GetRequestManager(),
+                  new PageManager(),
+                  new PageScraper(),
+                  new PageItemScraper(),
+                  new PageItemExtendedManager(),
+                  new PageItemExtendedScraper(),
+                  new RunIdManager(),
+                  new BulletPointManager()
+                  ) { }
 
         // Methods
 
@@ -43,5 +80,5 @@ namespace NW.WIDJobs
 
 /*
     Author: numbworks@gmail.com
-    Last Update: 08.05.2021
+    Last Update: 09.05.2021
 */
