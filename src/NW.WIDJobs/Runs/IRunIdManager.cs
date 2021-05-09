89721bf53fe4bbebe9b829e27d5dfe7b73ffc69d
Rubèn <numbworks@gmail.com>
2021-05-09 15:25:18 +0200

    RunIdManager: added.

---
+++ src/NW.WIDJobs/Runs/IRunIdManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NW.WIDJobs
+{
+    public interface IRunIdManager
+    {
+        string Create(DateTime now);
+        string Create(DateTime now, DateTime untilDate);
+        string Create(DateTime now, DateTime startDate, DateTime endDate);
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 09.05.2021
+*/
---
+++ src/NW.WIDJobs/Runs/RunIdManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NW.WIDJobs
+{
+    public class RunIdManager : IRunIdManager
+    {
+
+        // Fields
+        // Properties
+        // Constructors
+        public RunIdManager() { }
+
+        // Methods (public)
+        public string Create(DateTime now)
+        {
+
+            return string.Format(
+                "ID:{0}",
+                now.ToString("yyyyMMddHHmmssfff")
+            );
+
+        }
+        public string Create(DateTime now, DateTime startDate, DateTime endDate)
+        {
+
+            return string.Format(
+                "{0}|FROM:{1}|TO:{2}",
+                Create(now),
+                startDate.ToString("yyyyMMddHH"),
+                endDate.ToString("yyyyMMddHH")
+            );
+
+        }
+        public string Create(DateTime now, DateTime untilDate)
+        {
+
+            return string.Format(
+                "{0}|UNTIL:{1}",
+                Create(now),
+                untilDate.ToString("yyyyMMddHH")
+            );
+
+        }
+
+        // Methods (private)
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 09.05.2021
+*/
