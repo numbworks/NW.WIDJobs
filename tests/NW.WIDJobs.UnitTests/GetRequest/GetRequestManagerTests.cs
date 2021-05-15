2c103cc1fd08b13d5fb5b74c236594290af457d4
Rubèn <numbworks@gmail.com>
2021-05-15 23:53:01 +0200

    GetRequestManagerTests: added.

---
+++ tests/NW.WIDJobs.UnitTests/GetRequest/GetRequestManagerTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NW.WIDJobs.UnitTests
+{
+    [TestFixture]
+    public class GetRequestManagerTests
+    {
+
+        // Fields
+        private static TestCaseData[] getRequestManagerExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new GetRequestManager(null, null, null, null, null, null)
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("httpWebRequestFactory").Message
+            ).SetArgDisplayNames($"{nameof(getRequestManagerExceptionTestCases)}_01")
+
+        };
+        private static TestCaseData[] sendExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new GetRequestManager().Send(null)
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("url").Message
+            ).SetArgDisplayNames($"{nameof(sendExceptionTestCases)}_01")
+
+        };
+
+        // SetUp
+        // Tests
+        [TestCaseSource(nameof(getRequestManagerExceptionTestCases))]
+        public void GetRequestManager_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        [TestCaseSource(nameof(sendExceptionTestCases))]
+        public void Send_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        // TearDown		
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 15.05.2021
+*/
