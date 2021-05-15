ee1735001472f5f56ab7f164cb9af2e86c926ff7
Rubèn <numbworks@gmail.com>
2021-05-15 23:22:24 +0200

    HttpWebRequestFactoryTests: added.

---
+++ tests/NW.WIDJobs.UnitTests/GetRequest/HttpWebRequestFactoryTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Net;
+
+namespace NW.WIDJobs.UnitTests
+{
+    [TestFixture]
+    public class HttpWebRequestFactoryTests
+    {
+
+        // Fields
+        // SetUp
+        // Tests
+        [Test]
+        public void Create_ShouldCreateAnObjectOfTypeHttpWebRequest_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            HttpWebRequest actual = new HttpWebRequestFactory().Create("http://www.someurl.com");
+
+            // Assert
+            Assert.IsInstanceOf<HttpWebRequest>(actual);
+
+        }
+
+        // TearDown		
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 15.05.2021
+*/
