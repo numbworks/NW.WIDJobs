8e9a537fb2dc9608d6c75924993233a62e92b4a4
Rubèn <numbworks@gmail.com>
2021-05-23 19:55:29 +0200

    WIDExplorerSettings: added <summaries> and #regions.

--- src/NW.WIDJobs/WIDExplorerSettings.cs
+++ src/NW.WIDJobs/WIDExplorerSettings.cs
@@ -1,17 +1,28 @@
-﻿namespace NW.WIDJobs
+﻿using System;
+
+namespace NW.WIDJobs
 {
+    /// <summary>Collects all the global settings required by this library.</summary>
     public class WIDExplorerSettings
     {
 
-        // Fields
-        // Properties
-        public static ushort DefaultParallelRequests = 3;
-        public static uint DefaultPauseBetweenRequestsMs = 25000; // 25 seconds
+        #region Fields
+        #endregion
+
+        #region Properties
+
+        public static ushort DefaultParallelRequests { get; } = 3;
+        public static uint DefaultPauseBetweenRequestsMs { get; } = 25000; // 25 seconds
 
         public ushort ParallelRequests { get; }
         public uint PauseBetweenRequestsMs { get; }
 
-        // Constructors
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="WIDExplorerSettings"/> instance.</summary>
+        /// <exception cref="ArgumentException"/>
         public WIDExplorerSettings(
             ushort parallelRequests,
             uint pauseBetweenRequestsMs
@@ -24,12 +35,17 @@
             PauseBetweenRequestsMs = pauseBetweenRequestsMs;
 
         }
+
+        /// <summary>Initializes a <see cref="WIDExplorerSettings"/> instance using default parameters.</summary>
         public WIDExplorerSettings()
             : this(
                   DefaultParallelRequests,
                   DefaultPauseBetweenRequestsMs) { }
 
-        // Methods
+        #endregion
+
+        #region Methods_public
+        #endregion
 
     }
 }
