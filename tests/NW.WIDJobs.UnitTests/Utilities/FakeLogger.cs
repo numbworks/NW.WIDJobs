cc25488edc752db94f1768b848b233985b96ff7f
Rubèn <numbworks@gmail.com>
2021-05-25 23:43:44 +0200

    FakeLogger: added.

---
+++ tests/NW.WIDJobs.UnitTests/Utilities/FakeLogger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace NW.WIDJobs.UnitTests
+{
+    public class FakeLogger
+    {
+
+        // Fields
+        // Properties
+        public List<string> Messages { get; }
+
+        // Constructors
+        public FakeLogger()
+        {
+
+            Messages = new List<string>();
+
+        }
+
+        // Methods (public)
+        public void Log(string message)
+            => Messages.Add(message);
+
+        // Methods (private)
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 17.09.2020
+*/
