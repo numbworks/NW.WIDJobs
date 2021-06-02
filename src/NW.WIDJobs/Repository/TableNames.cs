e05aded912c3571a31676907a1a46d30c2941cc7
Rubèn <numbworks@gmail.com>
2021-06-02 23:44:17 +0200

    PageItemEntity, TableNames: added.

---
+++ src/NW.WIDJobs/Repository/TableNames.cs
@@ -0,0 +1,17 @@
+﻿namespace NW.WIDJobs
+{
+    /// <summary>Collects the names of the database tables.</summary>
+    public enum TableNames
+    {
+
+        PageItems,
+        PageItemsExtended,
+        PageItemsExtendedBulletPoints
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 02.06.2021
+*/
