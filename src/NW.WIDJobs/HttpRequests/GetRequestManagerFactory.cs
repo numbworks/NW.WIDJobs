948ca48b8710b987a70b2a8db811c1f4298bcf2a
Rubèn <numbworks@gmail.com>
2021-06-26 01:48:56 +0200

    GetRequestManagerFactory, I*: added.

---
+++ src/NW.WIDJobs/HttpRequests/GetRequestManagerFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+
+namespace NW.WIDJobs
+{
+    /// <summary><inheritdoc cref="IGetRequestManagerFactory"/></summary>
+    public class GetRequestManagerFactory : IGetRequestManagerFactory
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="GetRequestManagerFactory"/> instance.</summary>
+        public GetRequestManagerFactory() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public IGetRequestManager Create(
+            WebHeaderCollection headers,
+            string contentType,
+            CookieContainer cookieContainer,
+            string userAgent,
+            Version protocolVersion
+            )
+        {
+
+            return
+                new GetRequestManager(
+                        new HttpWebRequestFactory(),
+                        headers,
+                        contentType,
+                        cookieContainer,
+                        userAgent,
+                        protocolVersion
+                );
+
+        }
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 25.06.2021
+*/
---
+++ src/NW.WIDJobs/HttpRequests/IGetRequestManagerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace NW.WIDJobs
+{
+    /// <summary>A factory for <see cref="IGetRequestManager"/>.</summary>
+    public interface IGetRequestManagerFactory
+    {
+
+        /// <summary>
+        /// Creates a <see cref="IGetRequestManager"/> instance with a default <see cref="IHttpWebRequestFactory"/>.
+        /// </summary>
+        IGetRequestManager Create(
+            WebHeaderCollection headers, 
+            string contentType, 
+            CookieContainer cookieContainer, 
+            string userAgent, 
+            Version protocolVersion
+            );
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 25.06.2021
+*/
