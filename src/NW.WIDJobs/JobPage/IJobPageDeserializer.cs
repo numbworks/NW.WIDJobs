fed0c392b297149ca8b75d25ed9ca60983a3235c
Rubèn <numbworks@gmail.com>
2021-06-26 02:02:59 +0200

    JobPageDeserializer, I*: added.

---
+++ src/NW.WIDJobs/JobPage/IJobPageDeserializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NW.WIDJobs
+{
+    /// <summary>A deserializer for <see href="http://www.workindenmark.dk">WorkInDenmark</see>'s job page responses.</summary>
+    public interface IJobPageDeserializer
+    {
+
+        /// <summary>
+        /// Extracts the total amount of job postings from a <see href="http://www.workindenmark.dk">WorkInDenmark</see>'s job page response.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        ushort GetTotalJobPostings(string response);
+    
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 25.06.2021
+*/
---
+++ src/NW.WIDJobs/JobPage/JobPageDeserializer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+namespace NW.WIDJobs
+{
+    /// <inheritdoc cref="IJobPageDeserializer"/>
+    public class JobPageDeserializer : IJobPageDeserializer
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="JobPageDeserializer"/> instance.</summary>
+        public JobPageDeserializer() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public ushort GetTotalJobPostings(string response)
+        {
+
+            /*             
+                {
+                    ...
+                    "TotalResultCount": 2265
+                } 
+            */
+
+            Validator.ValidateStringNullOrWhiteSpace(response, nameof(response));
+
+            JsonSerializerOptions jso = new JsonSerializerOptions();
+            jso.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+
+            dynamic dyn = JsonSerializer.Deserialize<dynamic>(response, jso);
+            ushort totalJobPostings = ushort.Parse(dyn.fields["TotalResultCount"]);
+
+            return totalJobPostings;
+
+        }
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 25.06.2021
+*/
