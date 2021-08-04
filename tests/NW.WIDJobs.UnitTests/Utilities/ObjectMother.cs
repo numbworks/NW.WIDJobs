30c2df977880c991659a5f78000fb0ef31bdabc6
Rubèn <numbworks@gmail.com>
2021-08-04 12:29:46 +0200

    JobPage01_JobPosting*01: Program => ObjectMother.

--- tests/NW.WIDJobs.UnitTests/Utilities/ObjectMother.cs
+++ tests/NW.WIDJobs.UnitTests/Utilities/ObjectMother.cs
@@ -2045,6 +2045,73 @@ namespace NW.WIDJobs.UnitTests
 
         #endregion
 
+        /* -------------------------------------------- */
+
+        #region Shared_JobPage01
+
+        internal static string Shared_FakeResponse = "Fake response";
+        internal static string Shared_FakePurpose = "Fake purpose";
+
+        internal static JobPosting Shared_JobPage01_JobPosting01
+            = new JobPosting(
+                    runId: Shared_FakeRunId,
+                    pageNumber: 1,
+                    response: "{\r\n            \"AutomatchType\": 0,\r\n            \"Abroad\": false,\r\n            \"Weight\": 1.0,\r\n            \"Title\": \"Linux Specialist\",\r\n            \"JobHeadline\": \"Linux Specialist\",\r\n            \"Presentation\": \"<p>PARKEN, COPENHAGEN /</p>\\n<p>ENGINEERING – SYSTEM ADMINISTRATION /</p>\\n<p>FULL TIME, PERMANENT CONTRACT</p>\\n\",\r\n            \"HiringOrgName\": \"Keepit A/S\",\r\n            \"WorkPlaceAddress\": \"Per Henrik Lings Allé 4 7\",\r\n            \"WorkPlacePostalCode\": \"2100\",\r\n            \"WorkPlaceCity\": \"København Ø\",\r\n            \"WorkPlaceOtherAddress\": false,\r\n            \"WorkPlaceAbroad\": false,\r\n            \"WorkPlaceNotStatic\": false,\r\n            \"UseWorkPlaceAddressForJoblog\": true,\r\n            \"PostingCreated\": \"2021-07-02T00:00:00\",\r\n            \"LastDateApplication\": \"2021-08-27T00:00:00\",\r\n            \"FormattedLastDateApplication\": \"27. august 2021\",\r\n            \"AssignmentStartDate\": \"0001-01-01T00:00:00\",\r\n            \"IsHotjob\": false,\r\n            \"IsExternal\": false,\r\n            \"Url\": \"https://job.jobnet.dk/CV/FindWork/Details/5332213\",\r\n            \"Region\": \"Hovedstaden og Bornholm\",\r\n            \"Municipality\": \"København\",\r\n            \"Country\": \"Danmark\",\r\n            \"PostalCode\": \"2100\",\r\n            \"PostalCodeName\": null,\r\n            \"JobAnnouncementType\": \"\",\r\n            \"EmploymentType\": \"\",\r\n            \"WorkHours\": \"Fuldtid\",\r\n            \"OccupationArea\": \"\",\r\n            \"OccupationGroup\": \"\",\r\n            \"Occupation\": \"Programmør og systemudvikler\",\r\n            \"Location\": {\r\n                \"Latitude\": 55.7038,\r\n                \"Longitude\": 12.5721\r\n            },\r\n            \"JoblogWorkTime\": {\r\n                \"WorkHour\": null,\r\n                \"DailyWorkTime\": null\r\n            },\r\n            \"WorkplaceID\": 133543,\r\n            \"OrganisationId\": \"117090\",\r\n            \"HiringOrgCVR\": 30806883,\r\n            \"UserLoggedIn\": false,\r\n            \"AnonymousEmployer\": false,\r\n            \"ShareUrl\": \"https://job.jobnet.dk/CV/FindWork/DetailsSocialmedia/5332213\",\r\n            \"DetailsUrl\": \"https://job.jobnet.dk/CV/FindWork/DetailsWidk/5332213\",\r\n            \"JobLogUrl\": \"https://job.jobnet.dk/CV/FindWork/Details/5332213\",\r\n            \"HasLocationValues\": true,\r\n            \"ID\": \"5332213\",\r\n            \"Latitude\": 55.7038,\r\n            \"Longitude\": 12.5721\r\n        }",
+                    title: "Linux Specialist",
+                    presentation: "<p>PARKEN, COPENHAGEN /</p>\n<p>ENGINEERING – SYSTEM ADMINISTRATION /</p>\n<p>FULL TIME, PERMANENT CONTRACT</p>\n",
+                    hiringOrgName: "Keepit A/S",
+                    workPlaceAddress: "Per Henrik Lings Allé 4 7",
+                    workPlacePostalCode: 1200,
+                    workPlaceCity: "København Ø",
+                    postingCreated: new DateTime(2021, 07, 02),
+                    lastDateApplication: new DateTime(2021, 08, 27),
+                    url: "https://job.jobnet.dk/CV/FindWork/Details/5332213",
+                    region: "Hovedstaden og Bornholm",
+                    municipality: "København",
+                    country: "Danmark",
+                    employmentType: "",
+                    workHours: "Fuldtid",
+                    occupation: "Programmør og systemudvikler",
+                    workplaceId: 133543,
+                    organisationId: 117090,
+                    hiringOrgCVR: 30806883,
+                    id: 5332213,
+                    workPlaceCityWithoutZone: "København",
+                    jobPostingNumber: 1,
+                    jobPostingId: "5332213linuxspecialist"
+                );
+        internal static JobPostingExtended Shared_JobPage01_JobPostingExtended01
+            = new JobPostingExtended(
+                    jobPosting: Shared_JobPage01_JobPosting01,
+                    response: Shared_FakeResponse,                      // Ignored
+                    hiringOrgDescription: null,
+                    publicationStartDate: new DateTime(2021, 07, 02),
+                    publicationEndDate: new DateTime(2021, 08, 27),
+                    purpose: Shared_FakePurpose,                        // Ignored
+                    numberToFill: 1,
+                    contactEmail: "edc@keepit.com",
+                    contactPersonName: "Emil Daniel Christensen",
+                    employmentDate: null,
+                    applicationDeadlineDate: new DateTime(2021, 08, 27),
+                    bulletPoints: new HashSet<string>()
+                    {
+                        "-\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0Performance troubleshooting - if a service is not performing as expected, troubleshooting the process interactions on a live server in order to identify the root cause and propose a remedy, possibly in collaboration with the development team.",
+                        "-\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0Planning, testing, and executing Postgres database cluster migration from an older version to a newer version with little or no user-visible interruptions.",
+                        "-\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0\\u00A0Designing the next iteration of our network infrastructure for high-performance multi-site communication, and planning and executing the transition from the previous iteration with no customer visible downtime.\\u00A0"
+                    },
+                    bulletPointScenario: "keepit.com"
+                );
+
+        #endregion
+
+        #region Shared_JobPage02
+
+
+
+        #endregion
+
+        /* -------------------------------------------- */
+
         #region DbSetExtensionMethodsTests
 
         internal static DatabaseContext DbSetExtensionMethods_InMemoryDatabaseContext
