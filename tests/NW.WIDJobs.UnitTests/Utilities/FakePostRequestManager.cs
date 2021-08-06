54f910f805137a0a9f2b47deb1d9628f38cb1e3e
Rubèn <numbworks@gmail.com>
2021-08-06 14:44:52 +0200

    FakePostRequestManagerFactory, FakePostRequestManager: added.

---
+++ tests/NW.WIDJobs.UnitTests/Utilities/FakePostRequestManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace NW.WIDJobs.UnitTests
+{
+    /// <inheritdoc cref="IFakePostRequestManager"/>
+    public class FakePostRequestManager : IPostRequestManager
+    {
+
+        #region Fields
+
+        private string _response;
+
+        #endregion
+
+        #region Properties
+
+        public string ContentType
+            => throw new NotImplementedException();
+        public CookieContainer CookieContainer
+            => throw new NotImplementedException();
+        public WebHeaderCollection Headers
+            => throw new NotImplementedException();
+        public IHttpWebRequestFactory HttpWebRequestFactory
+            => throw new NotImplementedException();
+        public string Method
+            => throw new NotImplementedException();
+        public string PostData
+            => throw new NotImplementedException();
+        public Encoding PostDataEncoding
+            => throw new NotImplementedException();
+        public Version ProtocolVersion
+            => throw new NotImplementedException();
+        public string UserAgent
+            => throw new NotImplementedException();
+
+        #endregion
+
+        #region Constructors
+
+        public FakePostRequestManager(string response) 
+        {
+
+                _response = response;
+        
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        public string Send(string url)
+            => _response;
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 06.08.2021
+*/
---
+++ tests/NW.WIDJobs.UnitTests/Utilities/FakePostRequestManagerFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace NW.WIDJobs.UnitTests
+{
+
+    public class FakePostRequestManagerFactory : IPostRequestManagerFactory
+    {
+
+        #region Fields
+
+        private IPostRequestManager _postRequestManager;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        public FakePostRequestManagerFactory(string response) 
+        {
+
+            _postRequestManager = new FakePostRequestManager(response);
+
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        public IPostRequestManager Create
+            (WebHeaderCollection headers, string contentType, CookieContainer cookieContainer,
+            string userAgent, Version protocolVersion, string postData, Encoding postDataEncoding)
+            => _postRequestManager;
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 06.08.2021
+*/
