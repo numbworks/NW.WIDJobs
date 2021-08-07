df86b20c1fc8d7d9846ac6a33344dcd97131ded4
Rubèn <numbworks@gmail.com>
2021-08-07 20:51:05 +0200

    FakeGetRequestManager: added.

---
+++ tests/NW.WIDJobs.UnitTests/JobPostingExtended/JobPostingExtendedManagerTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NW.WIDJobs.UnitTests
+{
+    [TestFixture]
+    public class JobPostingExtendedManagerTests
+    {
+
+        #region Fields
+
+        private static TestCaseData[] jobPostingExtendedManagerExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new JobPostingExtendedManager(null, new JobPostingExtendedDeserializer())
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("getRequestManagerFactory").Message
+            ).SetArgDisplayNames($"{nameof(jobPostingExtendedManagerExceptionTestCases)}_01"),
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new JobPostingExtendedManager(new GetRequestManagerFactory(), null)
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("jobPostingExtendedDeserializer").Message
+            ).SetArgDisplayNames($"{nameof(jobPostingExtendedManagerExceptionTestCases)}_02")
+
+        };
+        private static TestCaseData[] jobPostingExtendedManagerTestCases =
+        {
+
+            new TestCaseData(
+                    "Some message",
+                    false
+                ).SetArgDisplayNames($"{nameof(jobPostingExtendedManagerTestCases)}_01")
+
+        };
+
+        #endregion
+
+        #region SetUp
+
+        #endregion
+
+        #region Tests
+
+        [TestCaseSource(nameof(jobPostingExtendedManagerExceptionTestCases))]
+        public void JobPostingExtendedManager_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        [TestCaseSource(nameof(jobPostingExtendedManagerTestCases))]
+        public void JobPostingExtendedManager_Should_When
+            (string message, bool expected)
+        {
+
+            // Arrange
+            // Act
+            // Assert
+
+        }
+
+        #endregion
+
+        #region TearDown
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 07.08.2021
+*/
---
+++ tests/NW.WIDJobs.UnitTests/Utilities/FakeGetRequestManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace NW.WIDJobs.UnitTests
+{
+    public class FakeGetRequestManager : IGetRequestManager
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public string ContentType 
+            => throw new NotImplementedException();
+        public CookieContainer CookieContainer 
+            => throw new NotImplementedException();
+        public WebHeaderCollection Headers 
+            => throw new NotImplementedException();
+        public IHttpWebRequestFactory HttpWebRequestFactory 
+            => throw new NotImplementedException();
+        public string Method 
+            => throw new NotImplementedException();
+        public Version ProtocolVersion 
+            => throw new NotImplementedException();
+        public string UserAgent 
+            => throw new NotImplementedException();
+
+        public string Response { get; private set; }
+        public string Url { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public FakeGetRequestManager(string response)
+        {
+
+            Response = response;
+
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        public string Send(string url, Encoding encoding = null)
+        {
+
+            Url = url;
+
+            return Response;
+
+        }
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 07.08.2021
+*/
