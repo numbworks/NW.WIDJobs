b767ff9e83183800f80d991101e6e72b948dbd9c
Rubèn <numbworks@gmail.com>
2021-08-07 20:59:01 +0200

    FakeGetRequestManagerFactory: added.

---
+++ tests/NW.WIDJobs.UnitTests/Utilities/FakeGetRequestManagerFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+
+namespace NW.WIDJobs.UnitTests
+{
+    public class FakeGetRequestManagerFactory : IGetRequestManagerFactory
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public FakeGetRequestManager FakeGetRequestManager { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public FakeGetRequestManagerFactory(FakeGetRequestManager fakeGetRequestManager) 
+        {
+
+            FakeGetRequestManager = fakeGetRequestManager;
+
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        public IGetRequestManager Create
+            (WebHeaderCollection headers, string contentType, CookieContainer cookieContainer, string userAgent, Version protocolVersion)
+        {
+
+            return FakeGetRequestManager;
+        
+        }
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 07.08.2021
+*/
