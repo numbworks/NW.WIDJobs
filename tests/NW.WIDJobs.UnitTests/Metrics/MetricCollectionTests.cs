12a79c2afda410006bfa93e783ceb0f4874c8761
Rubèn <numbworks@gmail.com>
2021-08-10 17:44:27 +0200

    MetricCollectionTests, fixed: "MetricCollection_ShouldInitializeANewMetricCollectionObject_WhenProperArguments()".

--- tests/NW.WIDJobs.UnitTests/Metrics/MetricCollectionTests.cs
+++ tests/NW.WIDJobs.UnitTests/Metrics/MetricCollectionTests.cs
@@ -1444,44 +1444,6 @@ namespace NW.WIDJobs.UnitTests
         {
 
             // Arrange
-            MetricCollection expected =
-                new MetricCollection(
-                        runId: ObjectMother.MetricCollection_ExplorationStage3_RunId,
-                        totalJobPages: ObjectMother.MetricCollection_ExplorationStage3_TotalJobPages,
-                        totalJobPostings: ObjectMother.MetricCollection_ExplorationStage3_TotalJobPostings,
-                        jobPostingsByHiringOrgName: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByHiringOrgName,
-                        jobPostingsByWorkPlaceAddress: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlaceAddress,
-                        jobPostingsByWorkPlacePostalCode: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlacePostalCode,
-                        jobPostingsByWorkPlaceCity: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlaceCity,
-                        jobPostingsByPostingCreated: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByPostingCreated,
-                        jobPostingsByLastDateApplication: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByLastDateApplication,
-                        jobPostingsByRegion: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByRegion,
-                        jobPostingsByMunicipality: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByMunicipality,
-                        jobPostingsByCountry: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByCountry,
-                        jobPostingsByEmploymentType: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByEmploymentType,
-                        jobPostingsByWorkHours: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkHours,
-                        jobPostingsByOccupation: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByOccupation,
-                        jobPostingsByWorkplaceId: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkplaceId,
-                        jobPostingsByOrganisationId: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByOrganisationId,
-                        jobPostingsByHiringOrgCVR: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByHiringOrgCVR,
-                        jobPostingsByWorkPlaceCityWithoutZone: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlaceCityWithoutZone,
-                        jobPostingsByPublicationStartDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByPublicationStartDate,
-                        jobPostingsByPublicationEndDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByPublicationEndDate,
-                        jobPostingsByNumberToFill: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByNumberToFill,
-                        jobPostingsByContactEmail: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByContactEmail,
-                        jobPostingsByContactPersonName: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByContactPersonName,
-                        jobPostingsByEmploymentDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByEmploymentDate,
-                        jobPostingsByApplicationDeadlineDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByApplicationDeadlineDate,
-                        jobPostingsByBulletPointScenario: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByBulletPointScenario,
-                        responseLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_ResponseLengthByJobPostingId,
-                        presentationLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_PresentationLengthByJobPostingId,
-                        extendedResponseLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_ExtendedResponseLengthByJobPostingId,
-                        hiringOrgDescriptionLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_HiringOrgDescriptionLengthByJobPostingId,
-                        purposeLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_PurposeLengthByJobPostingId,
-                        bulletPointsByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_BulletPointsByJobPostingId,
-                        totalBulletPoints: ObjectMother.MetricCollection_ExplorationStage3_TotalBulletPoints
-                    );
-
             // Act
             MetricCollection actual =
                 new MetricCollection(
@@ -1522,9 +1484,7 @@ namespace NW.WIDJobs.UnitTests
                     );
 
             // Assert
-            Assert.IsTrue(
-                    ObjectMother.AreEqual(expected, actual)
-                );
+            Assert.IsInstanceOf<MetricCollection>(actual);
 
         }
 
