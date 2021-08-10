230461793cd3043364293bec387982d82bf15d85
Rubèn <numbworks@gmail.com>
2021-08-10 17:39:19 +0200

    MetricCollectionTests: fixed.

--- tests/NW.WIDJobs.UnitTests/Metrics/MetricCollectionTests.cs
+++ tests/NW.WIDJobs.UnitTests/Metrics/MetricCollectionTests.cs
@@ -1428,50 +1428,7 @@ namespace NW.WIDJobs.UnitTests
                 ),
                 typeof(ArgumentNullException),
                 new ArgumentNullException("bulletPointsByJobPostingId").Message
-            ).SetArgDisplayNames($"{nameof(metricCollectionExceptionTestCases)}_33"),
-
-            new TestCaseData(
-                new TestDelegate(
-                    () => new MetricCollection(
-                                runId: ObjectMother.MetricCollection_ExplorationStage3_RunId,
-                                totalJobPages: ObjectMother.MetricCollection_ExplorationStage3_TotalJobPages,
-                                totalJobPostings: ObjectMother.MetricCollection_ExplorationStage3_TotalJobPostings,
-                                jobPostingsByHiringOrgName: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByHiringOrgName,
-                                jobPostingsByWorkPlaceAddress: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlaceAddress,
-                                jobPostingsByWorkPlacePostalCode: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlacePostalCode,
-                                jobPostingsByWorkPlaceCity: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlaceCity,
-                                jobPostingsByPostingCreated: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByPostingCreated,
-                                jobPostingsByLastDateApplication: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByLastDateApplication,
-                                jobPostingsByRegion: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByRegion,
-                                jobPostingsByMunicipality: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByMunicipality,
-                                jobPostingsByCountry: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByCountry,
-                                jobPostingsByEmploymentType: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByEmploymentType,
-                                jobPostingsByWorkHours: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkHours,
-                                jobPostingsByOccupation: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByOccupation,
-                                jobPostingsByWorkplaceId: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkplaceId,
-                                jobPostingsByOrganisationId: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByOrganisationId,
-                                jobPostingsByHiringOrgCVR: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByHiringOrgCVR,
-                                jobPostingsByWorkPlaceCityWithoutZone: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByWorkPlaceCityWithoutZone,
-                                jobPostingsByPublicationStartDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByPublicationStartDate,
-                                jobPostingsByPublicationEndDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByPublicationEndDate,
-                                jobPostingsByNumberToFill: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByNumberToFill,
-                                jobPostingsByContactEmail: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByContactEmail,
-                                jobPostingsByContactPersonName: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByContactPersonName,
-                                jobPostingsByEmploymentDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByEmploymentDate,
-                                jobPostingsByApplicationDeadlineDate: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByApplicationDeadlineDate,
-                                jobPostingsByBulletPointScenario: ObjectMother.MetricCollection_ExplorationStage3_JobPostingsByBulletPointScenario,
-                                responseLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_ResponseLengthByJobPostingId,
-                                presentationLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_PresentationLengthByJobPostingId,
-                                extendedResponseLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_ExtendedResponseLengthByJobPostingId,
-                                hiringOrgDescriptionLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_HiringOrgDescriptionLengthByJobPostingId,
-                                purposeLengthByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_PurposeLengthByJobPostingId,
-                                bulletPointsByJobPostingId: ObjectMother.MetricCollection_ExplorationStage3_BulletPointsByJobPostingId,
-                                totalBulletPoints: null
-                            )
-                ),
-                typeof(ArgumentNullException),
-                new ArgumentNullException("totalBulletPoints").Message
-            ).SetArgDisplayNames($"{nameof(metricCollectionExceptionTestCases)}_34")
+            ).SetArgDisplayNames($"{nameof(metricCollectionExceptionTestCases)}_33")
 
         };
 
