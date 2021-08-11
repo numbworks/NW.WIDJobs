ad0411ae67e814d7ec1039beda949a7c88f819a3
Rubèn <numbworks@gmail.com>
2021-08-11 20:20:53 +0200

    JobPostingManager, I*: added.

---
+++ src/NW.WIDJobs/JobPostings/IJobPostingManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NW.WIDJobs
+{
+    /// <summary>Collects all the helper methods related to <see cref="JobPosting"/>.</summary>
+    public interface IJobPostingManager
+    {
+
+        /// <summary>
+        /// During an exploration and while evaluating the content of a <see cref="JobPage"/>, 
+        /// this method establishes if the <paramref name="thresholdDate"/> condition is met 
+        /// and the exploration must stop ("True" case), or if the exploration 
+        /// must continue ("False" case).
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>      
+        bool IsThresholdConditionMet(DateTime thresholdDate, List<DateTime> postingCreatedCollection);
+
+        /// <summary>
+        /// If <see cref="IsThresholdConditionMet"/> returns "True" for a given <see cref="JobPage"/>, 
+        /// the exploration must stop and the unsuitable <see cref="JobPosting"/> objects must be removed.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>        
+        List<JobPosting> RemoveUnsuitable(DateTime thresholdDate, List<JobPosting> jobPostings);
+
+        /// <summary>
+        /// During an exploration and while evaluating the content of a <see cref="JobPage"/>, 
+        /// this method establishes if <paramref name="jobPostings"/> contains a <see cref="JobPosting"/> 
+        /// object with <see cref="JobPosting.JobPostingId"/> equal to <paramref name="jobPostingId"/>. 
+        /// If "True", the exploration must stop.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>      
+        bool IsThresholdConditionMet(string jobPostingId, List<JobPosting> jobPostings);
+
+        /// <summary>
+        /// If <see cref="IsThresholdConditionMet"/> returns "True" for a given <see cref="JobPosting.JobPostingId"/>, 
+        /// the exploration must stop and the unsuitable <see cref="JobPosting"/> objects must be removed.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>        
+        List<JobPosting> RemoveUnsuitable(string jobPostingId, List<JobPosting> jobPostings);
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 11.08.2021
+*/
---
+++ src/NW.WIDJobs/JobPostings/JobPostingManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NW.WIDJobs
+{
+    /// <inheritdoc cref="IJobPostingManager"/>
+    public class JobPostingManager : IJobPostingManager
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="JobPostingManager"/> instance.</summary>	
+        public JobPostingManager() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public bool IsThresholdConditionMet(DateTime thresholdDate, List<DateTime> postingCreatedCollection)
+        {
+
+            Validator.ValidateList(postingCreatedCollection, nameof(postingCreatedCollection));
+
+            DateTime mostRecent = postingCreatedCollection.OrderByDescending(createDate => createDate.Date).First();
+            DateTime leastRecent = postingCreatedCollection.OrderByDescending(createDate => createDate.Date).Reverse().First();
+
+            if (thresholdDate > leastRecent && thresholdDate <= mostRecent)
+                return true;
+
+            return false;
+
+        }
+        public List<JobPosting> RemoveUnsuitable(DateTime thresholdDate, List<JobPosting> jobPostings)
+        {
+
+            Validator.ValidateList(jobPostings, nameof(jobPostings));
+
+            List<JobPosting> subset = jobPostings.Where(pageItem => pageItem.PostingCreated >= thresholdDate).ToList();
+
+            return subset;
+
+        }
+
+        public bool IsThresholdConditionMet(string jobPostingId, List<JobPosting> jobPostings)
+        {
+
+            Validator.ValidateStringNullOrWhiteSpace(jobPostingId, nameof(jobPostingId));
+            Validator.ValidateList(jobPostings, nameof(jobPostings));
+
+            return jobPostings.Any(jobPosting => jobPosting.JobPostingId == jobPostingId);
+
+        }
+        public List<JobPosting> RemoveUnsuitable(string jobPostingId, List<JobPosting> jobPostings)
+        {
+
+            Validator.ValidateStringNullOrWhiteSpace(jobPostingId, nameof(jobPostingId));
+            Validator.ValidateList(jobPostings, nameof(jobPostings));
+
+            List<JobPosting> subset = new List<JobPosting>();
+            foreach (JobPosting jobPosting in jobPostings)
+                if (jobPosting.JobPostingId == jobPostingId)
+                    break;
+                else
+                    subset.Add(jobPosting);
+
+            return subset;
+
+        }
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 11.08.2021
+*/
