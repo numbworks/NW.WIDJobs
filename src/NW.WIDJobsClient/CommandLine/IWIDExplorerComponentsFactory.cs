3196804e7e7639697af1658ea0de586dc67ddcf3
Rubèn <numbworks@gmail.com>
2021-08-28 23:04:56 +0200

    IWIDExplorerComponentsFactory: added <summary>.

--- src/NW.WIDJobsClient/CommandLine/IWIDExplorerComponentsFactory.cs
+++ src/NW.WIDJobsClient/CommandLine/IWIDExplorerComponentsFactory.cs
@@ -2,10 +2,14 @@
 
 namespace NW.WIDJobsClient
 {
+    /// <summary>Factory for <see cref="WIDExplorerComponents"/>.</summary>
     public interface IWIDExplorerComponentsFactory
     {
 
+        /// <summary>Create a <see cref="WIDExplorerComponents"/> instance with default parameters.</summary>
         WIDExplorerComponents CreateDefault();
+
+        /// <summary>Create a <see cref="WIDExplorerComponents"/> instance for demo usage.</summary>
         WIDExplorerComponents CreateForDemoData();
 
     }
