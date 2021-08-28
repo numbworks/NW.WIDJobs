246b1b72f597b53b56cafd1de9ac32c78031e833
Rubèn <numbworks@gmail.com>
2021-08-28 23:02:06 +0200

    CommandLineManager, I*: added <summary>.

--- src/NW.WIDJobsClient/CommandLine/ICommandLineManager.cs
+++ src/NW.WIDJobsClient/CommandLine/ICommandLineManager.cs
@@ -1,8 +1,12 @@
 ﻿namespace NW.WIDJobsClient
 {
+    /// <summary>Groups all the logic related to the processing of the command-line arguments.</summary>
     public interface ICommandLineManager
     {
+
+        /// <summary>Processes the command-line arguments.</summary>
         int Execute(params string[] args);
+
     }
 }
 
