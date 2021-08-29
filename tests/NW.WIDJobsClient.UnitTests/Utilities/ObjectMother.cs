6a7c0edd7d19377035f9d825a873c46f7b00808d
Rubèn <numbworks@gmail.com>
2021-08-29 13:17:12 +0200

    ObjectMother: added.

---
+++ tests/NW.WIDJobsClient.UnitTests/Utilities/ObjectMother.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NW.WIDJobsClient.UnitTests
+{
+
+    public static class ObjectMother
+    {
+
+
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 29.08.2021
+*/
