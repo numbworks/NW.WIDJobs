6c4ecea70d4e605f8a17e6cc94977b3c5064dbce
Rubèn <numbworks@gmail.com>
2021-08-29 18:21:33 +0200

    WIDExplorerSettingsFactoryTests: added.

---
+++ tests/NW.WIDJobsClient.UnitTests/CommandLine/WIDExplorerSettingsFactoryTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using NW.WIDJobsClient.CommandLine;
+using NW.WIDJobs;
+
+namespace NW.WIDJobsClient.UnitTests
+{
+    [TestFixture]
+    public class WIDExplorerSettingsFactoryTests
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region SetUp
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void WIDExplorerSettingsFactory_ShouldCreateAnObjectOfTypeWIDExplorerSettingsFactory_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            WIDExplorerSettingsFactory actual = new WIDExplorerSettingsFactory();
+
+            // Assert
+            Assert.IsInstanceOf<WIDExplorerSettingsFactory>(actual);
+
+        }
+
+        [Test]
+        public void Create_ShouldCreateAnObjectOfTypeWIDExplorerSettings_WhenNoNullParameters()
+        {
+
+            // Arrange
+            // Act
+            WIDExplorerSettings actual 
+                = new WIDExplorerSettingsFactory()
+                    .Create(
+                        parallelRequests: WIDExplorerSettings.DefaultParallelRequests.ToString(),
+                        pauseBetweenRequestsMs: WIDExplorerSettings.DefaultPauseBetweenRequestsMs.ToString(),
+                        folderPath: WIDExplorerSettings.DefaultFolderPath,
+                        deleteAndRecreateDatabase: WIDExplorerSettings.DefaultDeleteAndRecreateDatabase
+                        );
+
+            // Assert
+            Assert.IsInstanceOf<WIDExplorerSettings>(actual);
+
+        }
+
+        [Test]
+        public void Create_ShouldCreateAnObjectOfTypeWIDExplorerSettings_WhenNullParameters()
+        {
+
+            // Arrange
+            // Act
+            WIDExplorerSettings actual = new WIDExplorerSettingsFactory().Create();
+
+            // Assert
+            Assert.IsInstanceOf<WIDExplorerSettings>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+
+        #endregion
+
+        #region Support_methods
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 29.08.2021
+*/
