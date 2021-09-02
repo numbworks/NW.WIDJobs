e5ef8cb360860f40ba607887c6e60142125312c7
Rubèn <numbworks@gmail.com>
2021-09-02 17:07:52 +0200

    HeaderFactory, I*: added.

---
+++ src/NW.WIDJobs/Headers/HeaderFactory.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+
+namespace NW.WIDJobs.Headers
+{
+    /// <inheritdoc cref="IHeaderFactory"/>
+    public class HeaderFactory : IHeaderFactory
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public static string DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0";
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="HeaderFactory"/> instance.</summary>	
+        public HeaderFactory() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public WebHeaderCollection Create()
+        {
+
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add(HttpRequestHeader.UserAgent, DefaultUserAgent);
+
+            return headers;
+
+        }
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 02.09.2021
+*/
---
+++ src/NW.WIDJobs/Headers/IHeaderFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace NW.WIDJobs.Headers
+{
+
+    public interface IHeaderFactory
+    {
+        /// <summary>Creates a collection of headers to attach to a HTTP Request.</summary>	
+        WebHeaderCollection Create();
+    
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 02.09.2021
+*/
