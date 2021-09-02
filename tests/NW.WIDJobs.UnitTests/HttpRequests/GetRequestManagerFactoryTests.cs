7912f9de5f44fd88b8bd0a4ebe2b11a67a38abd3
Rubèn <numbworks@gmail.com>
2021-09-02 21:22:34 +0200

    GetRequestManagerFactoryTests: added.

---
+++ tests/NW.WIDJobs.UnitTests/HttpRequests/GetRequestManagerFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using NW.WIDJobs.HttpRequests;
+
+namespace NW.WIDJobs.UnitTests
+{
+    [TestFixture]
+    public class GetRequestManagerFactoryTests
+    {
+
+        #region Fields
+        #endregion
+
+        #region SetUp
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void GetRequestManagerFactory_ShouldCreateAnObjectOfTypeIGetRequestManager_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            IGetRequestManager actual = new GetRequestManagerFactory().Create(null, null, null, null);
+
+            // Assert
+            Assert.IsInstanceOf<IGetRequestManager>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 02.09.2021
+*/
