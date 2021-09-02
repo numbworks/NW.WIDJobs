c40ce0b7680c016231713a7b85b5ad9479547a85
Rubèn <numbworks@gmail.com>
2021-09-02 17:33:16 +0200

    HeaderFactoryTests: added.

---
+++ tests/NW.WIDJobs.UnitTests/Headers/HeaderFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using NUnit.Framework;
+using NW.WIDJobs.Headers;
+
+namespace NW.WIDJobs.UnitTests
+{
+    [TestFixture]
+    public class HeaderFactoryTests
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region SetUp
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Create_ShouldCreateExpectedWebHeaderCollection_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            WebHeaderCollection actual = new HeaderFactory().Create();
+
+            // Assert
+            Assert.AreEqual(HeaderFactory.DefaultUserAgent, actual[HttpRequestHeader.UserAgent]);
+
+        }
+
+        #endregion
+
+        #region TearDown
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 02.09.2021
+*/
