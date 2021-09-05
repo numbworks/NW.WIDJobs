6ed78da3ec623f7e3c708d51753c55611f90ba44
Rubèn <numbworks@gmail.com>
2021-09-05 16:30:30 +0200

    Formatter, I*: completed.

--- src/NW.WIDJobs/Formatting/Formatter.cs
+++ src/NW.WIDJobs/Formatting/Formatter.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections;
 using NW.WIDJobs.Explorations;
 using NW.WIDJobs.Validation;
 using NW.WIDJobs.Metrics;
 using NW.WIDJobs.Messages;
+using NW.WIDJobs.JobPostingsExtended;
+using NW.WIDJobs.JobPostings;
 
 namespace NW.WIDJobs.Formatting
 {
     /// <inheritdoc cref="IFormatter"/>
-    public class Formatter
+    public class Formatter : IFormatter
     {
 
         #region Fields
@@ -19,14 +20,16 @@ namespace NW.WIDJobs.Formatting
 
         #region Properties
 
-        public static string TotalResultCountSynonym { get; } = "TotalJobPostings";
+        public static string NullString { get; } = "null";
+        public static string BulletPointsString { get; } = "BulletPoints";
 
         #endregion
 
         #region Constructors
 
-        /// <summary>Initializes a <see cref="Formatter"/> instance.</summary>	
-        public Formatter(IMetricCollectionManager metricCollectionManager) 
+        /// <summary>Initializes a <see cref="Formatter"/> instance.</summary>
+        /// <exception cref="ArgumentNullException"/>
+        public Formatter(IMetricCollectionManager metricCollectionManager)
         {
 
             Validator.ValidateObject(metricCollectionManager, nameof(metricCollectionManager));
@@ -43,21 +46,74 @@ namespace NW.WIDJobs.Formatting
 
         #region Methods_public
 
+        public string Format(JobPosting jobPosting)
+        {
+
+            Validator.ValidateObject(jobPosting, nameof(jobPosting));
+
+            return string.Concat(
+                        $"{nameof(JobPosting)}: ",
+                        $"'{jobPosting.JobPostingId}', ",
+                        $"'{jobPosting.HiringOrgName}', ",
+                        $"'{jobPosting.WorkPlaceCityWithoutZone}'."
+                    );
+
+        }
+        public string Format(JobPostingExtended jobPostingExtended)
+        {
+
+            Validator.ValidateObject(jobPostingExtended, nameof(jobPostingExtended));
+
+            return string.Concat(
+                        $"{nameof(JobPostingExtended)}: ",
+                        $"'{jobPostingExtended.JobPosting.JobPostingId}', ",
+                        $"'{jobPostingExtended.JobPosting.HiringOrgName}', ",
+                        $"'{jobPostingExtended.JobPosting.WorkPlaceCityWithoutZone}', ",
+                        $"'{(uint)jobPostingExtended.BulletPoints.Count}' {BulletPointsString}."
+                    );
+
+        }
         public string Format(Exploration exploration)
         {
 
             Validator.ValidateObject(exploration, nameof(exploration));
 
             if (exploration.Stage == Stages.Stage1_OnlyMetrics)
-                return $"{nameof(Exploration)}: '{TotalResultCountSynonym}':'{exploration.TotalResultCount}', '{nameof(exploration.TotalJobPages)}':'{exploration.TotalJobPages}'.";
+                return string.Concat(
+                            $"{nameof(Exploration)}: ",
+                            $"'{exploration.TotalResultCount}' {nameof(Exploration.JobPostings)}, ",
+                            $"'{exploration.TotalJobPages}' {nameof(Exploration.JobPages)}."
+                        );
 
             if (exploration.Stage == Stages.Stage2_UpToAllJobPostings)
-                return $"{nameof(Exploration)}: '{nameof(JobPages)}':'{exploration.JobPages?.Count.ToString() ?? "null"}', '{nameof(JobPostings)}':'{exploration.JobPostings?.Count.ToString() ?? "null"}'.";
+                return string.Concat(
+                            $"{nameof(Exploration)}: ",
+                            $"'{exploration.JobPages?.Count.ToString() ?? NullString}' {nameof(Exploration.JobPages)}, ",
+                            $"'{exploration.JobPostings?.Count.ToString() ?? NullString}' {nameof(Exploration.JobPostings)}."
+                        );
 
             if (exploration.Stage == Stages.Stage3_UpToAllJobPostingsExtended)
-                return $"{nameof(Exploration)}: '{nameof(JobPages)}':'{exploration.JobPages?.Count.ToString() ?? "null"}', '{nameof(JobPostingsExtended)}':'{exploration.JobPostingsExtended?.Count.ToString() ?? "null"}', 'TotalBulletPoints':'{_metricCollectionManager.TrySumBulletPoints(exploration)?.ToString() ?? "null"}'.";
+                return string.Concat(
+                            $"{nameof(Exploration)}: ",
+                            $"'{exploration.JobPages?.Count.ToString() ?? NullString}' {nameof(Exploration.JobPages)}, ",
+                            $"'{exploration.JobPostingsExtended?.Count.ToString() ?? NullString}' {nameof(Exploration.JobPostingsExtended)}, ",
+                            $"'{_metricCollectionManager.TrySumBulletPoints(exploration)?.ToString() ?? NullString}' {BulletPointsString}."
+                        );
 
             throw new Exception(MessageCollection.Formatter_NoFormattingStrategyAvailableFor(exploration.Stage.ToString()));
+
+        }
+        public string Format(MetricCollection metricCollection)
+        {
+
+            Validator.ValidateObject(metricCollection, nameof(metricCollection));
+
+            return string.Concat(
+                        $"{nameof(MetricCollection)}: ",
+                        $"'{metricCollection.TotalJobPages}' {nameof(Exploration.JobPages)}, ",
+                        $"'{metricCollection.TotalJobPostings}' {nameof(Exploration.JobPostings)}, ",
+                        $"'{metricCollection.TotalBulletPoints}' {BulletPointsString}."
+                    );
 
         }
 
---
+++ src/NW.WIDJobs/Formatting/IFormatter.cs
@@ -0,0 +1,30 @@
+﻿using NW.WIDJobs.Explorations;
+using NW.WIDJobs.JobPostings;
+using NW.WIDJobs.JobPostingsExtended;
+using NW.WIDJobs.Metrics;
+
+namespace NW.WIDJobs.Formatting
+{
+    /// <summary>Collects all the methods to format the library's objects.</summary>	
+    public interface IFormatter
+    {
+
+        /// <summary>Returns a compact and readable textual representation of the provided <paramref name="exploration"/>.</summary>	
+        string Format(Exploration exploration);
+
+        /// <summary>Returns a compact and readable textual representation of the provided <paramref name="jobPosting"/>.</summary>	
+        string Format(JobPosting jobPosting);
+
+        /// <summary>Returns a compact and readable textual representation of the provided <paramref name="jobPostingExtended"/>.</summary>	
+        string Format(JobPostingExtended jobPostingExtended);
+
+        /// <summary>Returns a compact and readable textual representation of the provided <paramref name="metricCollection"/>.</summary>	
+        string Format(MetricCollection metricCollection);
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 05.09.2021
+*/
