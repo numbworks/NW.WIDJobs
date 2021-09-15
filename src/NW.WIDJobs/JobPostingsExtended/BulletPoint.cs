f9ece46cc5c688df1ebfe685ae54f8b658188ee1
Rubèn <numbworks@gmail.com>
2021-09-15 16:47:29 +0200

    BulletPoint: added.

---
+++ src/NW.WIDJobs/JobPostingsExtended/BulletPoint.cs
@@ -0,0 +1,49 @@
+﻿using NW.WIDJobs.Validation;
+
+namespace NW.WIDJobs.JobPostingsExtended
+{
+    /// <summary>Initializes a bullet point for <see cref="JobPostingExtended"/>.</summary>
+    public class BulletPoint
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public string Text { get; }
+        public string Type { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="BulletPoint"/> instance.</summary>	
+        public BulletPoint(string text, string type) 
+        {
+
+            Validator.ValidateObject(text, nameof(text));
+
+            Text = text;
+            Type = type;
+
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 15.09.2021
+*/
