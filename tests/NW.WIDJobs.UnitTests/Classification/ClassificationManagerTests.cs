28b4ca5367f374cfc72ea2ec62b9dccbe9ad81f7
Rubèn <numbworks@gmail.com>
2021-09-15 13:41:04 +0200

    ClassificationManagerTests, I iteration.

---
+++ tests/NW.WIDJobs.UnitTests/Classification/ClassificationManagerTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using NW.WIDJobs.Classification;
+using System.Collections.Generic;
+using NW.NGramTextClassification;
+
+namespace NW.WIDJobs.UnitTests
+{
+    [TestFixture]
+    public class ClassificationManagerTests
+    {
+
+        #region Fields
+
+        private static TestCaseData[] classificationManagerExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new ClassificationManager(null)
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("textClassifier").Message
+            ).SetArgDisplayNames($"{nameof(classificationManagerExceptionTestCases)}_01")
+
+        };
+
+        #endregion
+
+        #region SetUp
+
+        #endregion
+
+        #region Tests
+
+        [TestCaseSource(nameof(classificationManagerExceptionTestCases))]
+        public void ClassificationManager_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        [Test]
+        public void ClassificationManager_ShouldCreateAnObjectOfTypeClassificationManager_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            ClassificationManager actual = new ClassificationManager();
+
+            // Assert
+            Assert.IsInstanceOf<ClassificationManager>(actual);
+
+        }
+
+        [Test]
+        public void GetPreLabeledExamplesForLanguage_ShouldCreateAnObjectOfTypeListLabeledExample_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            List<LabeledExample> actual = new ClassificationManager().GetPreLabeledExamplesForLanguage();
+
+            // Assert
+            Assert.IsInstanceOf<List<LabeledExample>>(actual);
+
+        }
+
+        [Test]
+        public void GetPreLabeledExamplesForBulletPointType_ShouldCreateAnObjectOfTypeListLabeledExample_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            List<LabeledExample> actual = new ClassificationManager().GetPreLabeledExamplesForBulletPointType();
+
+            // Assert
+            Assert.IsInstanceOf<List<LabeledExample>>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 15.09.2021
+*/
